a58e4346b377ff1c40c210d7ef28d746370dbc77
Harshul Singla <VE00YM337@YMI.COM>
2022-03-08 08:37:42 +0530

    second commit

--- src/main/java/com/bankApp/web/controller/HomeController.java
+++ src/main/java/com/bankApp/web/controller/HomeController.java
@@ -1,10 +1,20 @@
 package com.bankApp.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.bankApp.model.service.AccountService;
+
 @Controller
 public class HomeController {
+
+	private AccountService accountService;
+	
+	@Autowired
+	public HomeController(AccountService accountService) {
+		this.accountService=accountService;
+	}
 	@GetMapping("/")
 	public String home() {
 		return "home";
